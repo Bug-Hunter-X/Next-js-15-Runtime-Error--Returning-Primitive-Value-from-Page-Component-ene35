18ff6c112fcde072ff7ba71a8c1f02dd225e7a77
BugHunter-X <askzenai@gmail.com>
2024-12-14 05:32:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,24 @@
+```javascript
+// pages/index.js
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app!</h1>
+      <Link href="/about">
+        <a>Go to About page</a>
+      </Link>
+    </div>
+  );
+}
+```
+```javascript
+// pages/about.js
+
+export default function About() {
+  // This will cause a runtime error in Next.js 15
+  // because it's not wrapped in a React component
+  return 'This is the about page';
+}
+```
